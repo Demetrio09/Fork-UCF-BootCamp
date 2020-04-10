e61bb1224d78c762244b18a7b9476574e8dd69f0
Jamie DeLong <jdlong1980@gmail.com>
2020-04-09 21:40:04 -0400

    push solved for 4.2

---
+++ Class-Content/04-week/4.2/17-Stu_Event_Bubbling/Unsolved/script.js
@@ -0,0 +1,11 @@
+var images = [
+    "https://picsum.photos/300/200",
+    "https://picsum.photos/300/201",
+    "https://picsum.photos/300/202",
+    "https://picsum.photos/300/203"
+];
+
+var carouselEl = document.querySelector(".carouselbox");
+var nextEl = carousel.querySelector(".next");
+var prevEl = carousel.querySelector(".prev");
+carouselEl.style.backgroundImage = "url('https://picsum.photos/300/200')";
